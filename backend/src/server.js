2b2cbd7115a89f1fafc3ac5e29531785770d50b7
Ulduz <ouldouz.weddingly@gmail.com>
2025-07-01 06:10:25 +0330

    Prepare for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,84 +1,47 @@
 import express from "express";
-//const express= require("express")
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDb } from "./config/db.js";
-import dontenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
-import cors from "cors";
 
-dontenv.config();
+dotenv.config();
 
-const app=express();
-const PORT= process.env.PORT || 5001
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-
-//middleware
-app.use(cors({
-    origin:"http://localhost:5173",
-}));
-app.use(express.json()); // This middleware will parse json bodies: req.body
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-// Our simple custom middle ware
-// app.use((req,res,next)=>{
-//     console.log(`Req method is ${req.method} and Req url is ${req.url}`);
-//     next();
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
 // });
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDb().then(() => {
-    app.listen(PORT, () =>{
+  app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT);
+  });
 });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.get("/api/notes", (req,res) => {
-//     res.status(200).send("You have got 30 notes");
-// });
-
-// app.post("/api/notes", (req,res) => {
-//     res.status(201).json({message:"Note created successfully!"});
-// });
-
-// app.put("/api/notes/:id", (req,res) => {
-//     res.status(200).json({message:"Note updated successfully!"});
-// });
-
-// app.delete("/api/notes/:id", (req,res) => {
-//     res.status(200).json({message:"Note deleted successfully!"});
-// });
-
-
-
-//mongodb+srv://upakpoor:KCPwpQeNWlNBO57E@cluster0.c4nusor.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
